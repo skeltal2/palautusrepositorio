0276519cded6e453f78f93bd098e4ca3db4b1d90
Joel Weinitschke <joel.weinitschke@gmail.com>
2023-11-13 17:07:03 +0200

    viikko 2 teht 1 ja 2

---
+++ viikko2/project-reader/src/project.py
@@ -0,0 +1,21 @@
+class Project:
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
+        self.name = name
+        self.description = description
+        self.license = license
+        self.authors = authors
+        self.dependencies = dependencies
+        self.dev_dependencies = dev_dependencies
+
+    def _stringify_dependencies(self, dependencies):
+        return "\n- " + "\n- ".join(dependencies) if len(dependencies) > 0 else "\n-"
+
+    def __str__(self):
+        return (
+            f"Name: {self.name}"
+            f"\nDescription: {self.description or '-'}"
+            f"\nLicense: {self.license}\n"
+            f"\nAuthors: {self._stringify_dependencies(self.authors)}\n"
+            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}\n"
+            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+        )
